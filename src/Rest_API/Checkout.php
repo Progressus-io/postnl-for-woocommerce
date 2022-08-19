418803cc14fd6f991871f2d58427dc828fa9a7f1
Iyut <iyut85@yahoo.com>
2022-08-19 21:12:59 +0700

    remove request args example

--- src/Rest_API/Checkout.php
+++ src/Rest_API/Checkout.php
@@ -29,56 +29,6 @@ class Checkout extends Base {
 	 */
 	public function send_request() {
 		$api_url      = esc_url( $this->get_api_url() );
-
-		/*
-		$request_args = array(
-			'method'  => 'POST',
-			'headers' => $this->get_headers_args(),
-			'body'    => wp_json_encode(
-				array(
-					'OrderDate'        => '31-07-2022 23:00:00',
-					'ShippingDuration' => 2,
-					'CutOffTimes'      => array(
-						array(
-							'Day'       => '00',
-							'Available' => true,
-							'Type'      => 'Regular',
-							'Time'      => '23:00:00',
-						),
-						array(
-							'Day'       => '07',
-							'Available' => false,
-							'Type'      => 'Regular',
-						),
-					),
-					'HolidaySorting'   => true,
-					'Options'          => array(
-						'Daytime',
-						'Evening',
-						'Pickup',
-					),
-					'Locations'        => 3,
-					'Days'             => 5,
-					'Addresses'        => array(
-						array(
-							'AddressType' => '01',
-							'Street'      => 'Molengraaffplantsoen',
-							'HouseNr'     => '74',
-							'HouseNrExt'  => 'bis',
-							'Zipcode'     => '3571ZZ',
-							'City'        => 'Utrecht',
-							'CountryCode' => 'NL',
-						),
-						array(
-							'AddressType' => '02',
-							'CountryCode' => 'NL',
-						),
-					),
-				)
-			),
-		);
-		*/
-
 		$request_args = array(
 			'method'  => 'POST',
 			'headers' => $this->get_headers_args(),

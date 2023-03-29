1ccce30640168ac536fbfa14c2c05e6e0b8daffc
Abdalsalaam Halawa <abdalsalaam@outlook.com>
2023-03-29 22:00:34 +0300

    Use function have_backend_data in Base class

--- src/Order/Base.php
+++ src/Order/Base.php
@@ -964,4 +964,22 @@ abstract class Base {
 			}
 		}
 	}
+
+	/**
+	 * Check if the order have the label data.
+	 *
+	 * @param WC_Order $order current order object.
+	 * @param String   $field Backend field name.
+	 *
+	 * @return boolean
+	 */
+	public function have_backend_data( $order, $field = '' ) {
+		$order_data = $order->get_meta( $this->meta_name );
+
+		if ( ! empty( $field ) ) {
+			return ! empty( $order_data['backend'][ $field ] );
+		}
+
+		return ! empty( $order_data['backend'] );
+	}
 }

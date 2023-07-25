6d371d2e4c838f6b8b6064b939b8b94c928b5d3f
sal4sup <sal4sup@gmail.com>
2023-07-26 01:32:30 +0300

    Check Insurance Amount Limit

--- src/Rest_API/Shipping/Item_Info.php
+++ src/Rest_API/Shipping/Item_Info.php
@@ -228,6 +228,9 @@ class Item_Info extends Base_Info {
 			'subtotal'       => $order->get_subtotal(),
 		);
 
+		// Check mailbox weight limit
+		$this->check_insurance_amount_limit( $this->api_args['backend_data'], $order->get_subtotal() );
+
 		foreach ( $order->get_items() as $item_id => $item ) {
 			$product = $item->get_product();
 
@@ -940,4 +943,22 @@ class Item_Info extends Base_Info {
 			);
 		}
 	}
+
+	/**
+	 * Check Insurance amount limit.
+	 *
+	 * @param $backend_data  .
+	 * @param $order_total  .
+	 *
+	 * @return void.
+	 * @throws \Exception if the order weight exceeds € 5000.
+	 */
+	protected function check_insurance_amount_limit( $backend_data, $order_total ) {
+		if ( 'yes' === $backend_data['insured_shipping'] && $order_total > 5000 ) {
+			throw new \Exception(
+				__( 'Insurance amount  is required and cannot exceed the maximum allowed amount (€ 5000) Your total is. ' . $order_total, 'postnl-for-woocommerce' )
+			);
+		}
+	}
+
 }

61327155a2a8bd93c74801be9c0c19ce9624d82a
Abdalsalaam Halawa <abdalsalaam@outlook.com>
2023-10-03 13:02:18 +0300

    fix changelog

--- postnl-for-woocommerce.php
+++ postnl-for-woocommerce.php
@@ -5,10 +5,10 @@
  * Description: With this plug-in you can easily confirm your PostNL shipments and print shipping labels in no time. In addition, your customers are more in control because they choose where and when they receive their order.
  * Author: PostNL
  * Author URI: https://postnl.post/
- * Version: 5.2.3
- * Tested up to: 6.2
+ * Version: 5.2.5
+ * Tested up to: 6.3
  * WC requires at least: 4.0
- * WC tested up to: 7.8
+ * WC tested up to: 8.1
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

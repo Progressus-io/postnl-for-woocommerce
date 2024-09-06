697fb4c6b9d7a11fce79bfaee20756a13b5915da
OlfatProgressus <olfat@progressus.io>
2024-09-06 15:19:16 +0300

    fix warnings

--- src/Order/OrdersList.php
+++ src/Order/OrdersList.php
@@ -107,12 +107,12 @@ class OrdersList extends Base {
 	 * @return array.
 	 */
 	public function add_order_delivery_date_column_header( $columns ) {
-		$wc_actions = isset( $columns['wc_actions'] ) ? $columns['wc_actions'] : '';
-		unset( $columns['wc_actions'] );
-
 		$columns['postnl_delivery_date'] = esc_html__( 'Delivery Date', 'postnl-for-woocommerce' );
-		$columns['wc_actions']           = $wc_actions;
-
+		if( isset( $columns['wc_actions'] ) ){
+			$wc_actions = $columns['wc_actions'];
+			unset( $columns['wc_actions'] );
+			$columns['wc_actions']       = $wc_actions;
+		}
 		return $columns;
 	}
 

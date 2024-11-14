8356a1ce6fee7b7907f9f36339d03fc085fe38f6
sal4sup <sal4sup@gmail.com>
2024-11-14 20:12:01 +0200

    Fill adress - build files

--- build/postnl-container-frontend.asset.php
+++ build/postnl-container-frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wc-blocks-checkout', 'wc-settings', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '8b9bf7ba8ec19ddd5947');
+<?php return array('dependencies' => array('lodash', 'react', 'wc-blocks-checkout', 'wc-settings', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b52f69cb35d407c12b82');

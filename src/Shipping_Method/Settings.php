ca16fc611415bcec22e9cacb25d177a245a6fb50
OlfatProgressus <olfat@progressus.io>
2025-01-08 11:47:17 +0200

    Allow different postcodes and city for return addresses

--- src/Shipping_Method/Settings.php
+++ src/Shipping_Method/Settings.php
@@ -212,6 +212,20 @@ class Settings extends \WC_Settings_API {
 				'for_country' => array( 'NL' ),
 				'class'       => 'country-nl',
 			),
+			'freepost_zip'             => array(
+				'title'       => esc_html__( 'Freepost Zipcode', 'postnl-for-woocommerce' ),
+				'type'        => 'text',
+				'description' => esc_html__( 'Enter Freepost Zipcode.', 'postnl-for-woocommerce' ),
+				'desc_tip'    => true,
+				'default'     => '',
+			),
+			'freepost_city'            => array(
+				'title'       => esc_html__( 'Freepost City', 'postnl-for-woocommerce' ),
+				'type'        => 'text',
+				'description' => esc_html__( 'Enter Freepost City.', 'postnl-for-woocommerce' ),
+				'desc_tip'    => true,
+				'default'     => '',
+			),
 			'return_address_street'          => array(
 				'title'       => esc_html__( 'Street Address', 'postnl-for-woocommerce' ),
 				'type'        => 'text',
@@ -828,6 +842,24 @@ class Settings extends \WC_Settings_API {
 	 */
 	public function get_return_reply_number() {
 		return $this->get_country_option( 'return_replynumber', '' );
+	}
+
+	/**
+	 * Get freepost city from the settings.
+	 *
+	 * @return String
+	 */
+	public function get_freepost_city() {
+		return $this->get_country_option( 'freepost_city', '' );
+	}
+
+	/**
+	 * Get freepost zipcode from the settings.
+	 *
+	 * @return String
+	 */
+	public function get_freepost_zipcode() {
+		return $this->get_country_option( 'freepost_zip', '' );
 	}
 
 	/**

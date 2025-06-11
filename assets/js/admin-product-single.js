72cce5b7d308132bd0e49b60441fc018c63efc3e
sal4sup <sal4sup@gmail.com>
2025-06-12 01:31:07 +0300

    Update Js code to work with both product and block editor

--- assets/js/admin-product-single.js
+++ assets/js/admin-product-single.js
@@ -10,24 +10,34 @@
 		 * Prevent both "18+" and "Letterbox Parcel" checkboxes from being checked at the same time.
 		 */
 		prevent_conflicting_checkboxes: function () {
-			var $adultCheckbox = $('input[name="_postnl_adult_product"]');
-			var $letterboxCheckbox = $('input[name="_postnl_letterbox_parcel"]');
 
-			if ( ! $adultCheckbox.length || ! $letterboxCheckbox.length ) {
-				return;
+			const adultSelector =
+				'input[name="_postnl_adult_product"], ' +
+				'[data-template-block-id="postnl-adult-product"] .components-checkbox-control__input';
+
+			const letterboxSelector =
+				'input[name="_postnl_letterbox_parcel"], ' +
+				'[data-template-block-id="postnl-letterbox-parcel"] .components-checkbox-control__input';
+
+			if ($(adultSelector).is(':checked')) {
+				$(letterboxSelector).prop('checked', false);
+			}
+			if ($(letterboxSelector).is(':checked')) {
+				$(adultSelector).prop('checked', false);
 			}
 
-			$adultCheckbox.on( 'change', function () {
-				if ( $( this ).is( ':checked' ) ) {
-					$letterboxCheckbox.prop( 'checked', false );
-				}
-			} );
-
-			$letterboxCheckbox.on( 'change', function () {
-				if ( $( this ).is( ':checked' ) ) {
-					$adultCheckbox.prop( 'checked', false );
-				}
-			} );
+			$(document)
+				.off('change.postnlConflict')
+				.on('change.postnlConflict', adultSelector, function () {
+					if ($(this).is(':checked')) {
+						$(letterboxSelector).prop('checked', false);
+					}
+				})
+				.on('change.postnlConflict', letterboxSelector, function () {
+					if ($(this).is(':checked')) {
+						$(adultSelector).prop('checked', false);
+					}
+				});
 		}
 	};
 

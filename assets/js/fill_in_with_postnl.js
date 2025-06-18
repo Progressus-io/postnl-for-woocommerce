218684f74cb67e8ebde0d8fdac29a79f14d7de12
OlfatProgressus <olfat@progressus.io>
2025-06-18 16:28:30 +0300

    show errors as wc notices

--- assets/js/fill_in_with_postnl.js
+++ assets/js/fill_in_with_postnl.js
@@ -7,39 +7,37 @@
 		},
 
         handle_login_button: function () {
-            const button = $( '#postnl-login-button' );
-            if ( button.length ) {
-                button.on( 'click', function (e) {
-                    e.preventDefault();
+            $( document ).on( 'click', '#postnl-login-button', function ( e ) {
+                e.preventDefault();
 
-                    button.prop( 'disabled', true );
+                const button = $( this );
+                button.prop( 'disabled', true );
 
-                    $.ajax( {
-                        url: postnlCheckoutParams.rest_url,
-                        method: 'POST',
-                        data: {
-                            nonce: postnlCheckoutParams.nonce
-                        },
-                        success: function ( response ) {
-                            if ( response.success && response.data.redirect_uri ) {
-                                window.location.href = response.data.redirect_uri;
-                            } else {
-                                alert( __( 'Failed to initiate PostNL login.', 'postnl-for-woocommerce' ) );  
-                            }
-                        },
-                        error: function ( response ) {
-                            alert( response.data.message );
-                        },
-                        complete: function () {
-                            button.prop( 'disabled', false );
+                $.ajax( {
+                    url: postnlCheckoutParams.rest_url,
+                    method: 'POST',
+                    data: {
+                        nonce: postnlCheckoutParams.nonce
+                    },
+                    success: function ( response ) {
+                        if ( response.success && response.data.redirect_uri ) {
+                            window.location.href = response.data.redirect_uri;
+                        } else {
+                            fill_in_with_postnl.show_notice( 'Failed to initiate PostNL login.', 'error' );
                         }
-                    } );
+                    },
+                    error: function ( response ) {
+                        const message = response.responseJSON?.data?.message || 'An unknown error occurred.';
+                        fill_in_with_postnl.show_notice( message, 'error' );
+                    },
+                    complete: function () {
+                        button.prop( 'disabled', false );
+                    }
                 } );
-            }
+            } );
         },
 
         prefill_checkout_fields: function () {
-
 			$.ajax( {
                 type: 'POST',
                 url: postnlCheckoutParams.ajax_url,
@@ -62,23 +60,44 @@
 						$( '#billing_address_1' ).val( primaryAddress.streetName ).trigger( 'change' );
                         $( '#billing_address_2' ).val( primaryAddress.houseNumberAddition ).trigger( 'change' );
                         $( '#billing_house_number' ).val( primaryAddress.houseNumber ).trigger( 'change' );
-						$( '#billing_postcode' ).val( primaryAddress.postalCode).trigger( 'change' );
-						$( '#billing_city' ).val( primaryAddress.cityName).trigger( 'change' );
+						$( '#billing_postcode' ).val( primaryAddress.postalCode ).trigger( 'change' );
+						$( '#billing_city' ).val( primaryAddress.cityName ).trigger( 'change' );
 						$( '#billing_country' ).val( primaryAddress.countryName ).trigger( 'change' );
-                        
+
                         $( '#shipping_address_1' ).val( primaryAddress.streetName ).trigger( 'change' );
                         $( '#shipping_address_2' ).val( primaryAddress.houseNumberAddition ).trigger( 'change' );
                         $( '#shipping_house_number' ).val( primaryAddress.houseNumber ).trigger( 'change' );
-						$( '#shipping_postcode' ).val( primaryAddress.postalCode).trigger( 'change' );
-						$( '#shipping_city' ).val( primaryAddress.cityName).trigger( 'change' );
+						$( '#shipping_postcode' ).val( primaryAddress.postalCode ).trigger( 'change' );
+						$( '#shipping_city' ).val( primaryAddress.cityName ).trigger( 'change' );
 						$( '#shipping_country' ).val( primaryAddress.countryName ).trigger( 'change' );
 					}
 				},
                 error: function ( response ) {
-                    alert( response.data.message );
+                    const message = response.responseJSON?.data?.message || 'An unknown error occurred.';
+                    fill_in_with_postnl.show_notice( message, 'error' );
                 }
 			} );
-		}
+		},
+
+        show_notice: function ( message, type = 'error' ) {
+            const noticeId = `postnl-notice-${type}`;
+            let noticesWrapper = $( '.woocommerce-notices-wrapper' ).first();
+
+            // Ensure wrapper exists
+            if ( ! noticesWrapper.length ) {
+                noticesWrapper = $( '<div class="woocommerce-notices-wrapper"></div>' );
+                $( '.woocommerce' ).prepend( noticesWrapper );
+            }
+
+            // Remove old notice
+            noticesWrapper.find( `#${noticeId}` ).remove();
+
+            // Add new notice
+            noticesWrapper.append(
+                `<div id="${noticeId}" class="woocommerce-${type}" data-postnl="1">${message}</div>`
+            );
+        }
+
 	};
 
 	fill_in_with_postnl.init();

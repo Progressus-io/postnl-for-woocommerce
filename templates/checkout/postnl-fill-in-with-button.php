776ab0dee0ff827ceb81572cdd09e39d3d36781d
OlfatProgressus <olfat@progressus.io>
2025-08-08 10:14:09 +0300

    add specific css class to the button in each page

--- templates/checkout/postnl-fill-in-with-button.php
+++ templates/checkout/postnl-fill-in-with-button.php
@@ -8,8 +8,17 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+$wrapper_classes = array();
+if ( is_cart() ) {
+	$wrapper_classes[] = 'postnl-button-in-cart';
+} elseif ( is_checkout() ) {
+	$wrapper_classes[] = 'postnl-button-in-checkout';
+} else {
+	$wrapper_classes[] = 'postnl-button-in-minicart';
+}
+
 ?>
-<div class="postnl-login-button__container">
+<div class="postnl-login-button__container <?php echo esc_attr( implode( ' ', $wrapper_classes ) ); ?>">
 	<button id="postnl-login-button" aria-label="<?php esc_html_e( 'Fill in with PostNL', 'postnl-for-woocommerce' ); ?>" href="#">
 		<img src="<?php echo $postnl_logo_url; ?>" alt="<?php esc_attr_e( 'PostNL Logo', 'postnl-for-woocommerce' ); ?>" id="postnl-logo" />
 		<span id="postnl-login-button__text">

a9d854edbb88cddac695ee300f48099ca8594ad8
OlfatProgressus <olfat@progressus.io>
2025-09-02 15:47:13 +0300

    Update postnl-for-woocommerce.php

--- postnl-for-woocommerce.php
+++ postnl-for-woocommerce.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: PostNL for WooCommerce
  * Plugin URI: https://github.com/Progressus-io/postnl-for-woocommerce/
- * Description: Confirm PostNL shipments, print labels, and let customers choose delivery time and location.
+ * Description: Confirm PostNL shipments, print shipping labels, and let customers choose delivery time and location.
  * Author: PostNL
  * Author URI: https://postnl.post/
  * License: GPLv2 or later

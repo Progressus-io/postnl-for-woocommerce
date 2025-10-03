faa3e4bdd327fda58fde3dd12d5084d2f038e231
OlfatProgressus <olfat@progressus.io>
2025-10-03 17:25:17 +0300

    exclude parcel vending machines for 18+ products

--- src/Frontend/Dropoff_Points.php
+++ src/Frontend/Dropoff_Points.php
@@ -7,6 +7,8 @@
 
 namespace PostNLWooCommerce\Frontend;
 
+use PostNLWooCommerce\Utils;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -72,11 +74,26 @@ class Dropoff_Points extends Base {
 		$pickup_points = $response['PickupOptions'];
 		$return_data   = $this->get_init_content_data( $post_data );
 
+		// Check if cart has an adult product.
+		$has_adult = false;
+		foreach ( WC()->cart->get_cart() as $cart_item ) {
+			$product = $cart_item['data'];
+			if ( $product && Utils::is_adults_only_product( $product ) ) {
+				$has_adult = true;
+				break;
+			}
+		}
+
 		foreach ( $pickup_points as $pickup_point ) {
 			$date = ! empty( $pickup_point['PickupDate'] ) ? $pickup_point['PickupDate'] : '';
 			$type = ! empty( $pickup_point['Option'] ) ? $pickup_point['Option'] : '';
 
 			if ( empty( $pickup_point['Locations'] ) ) {
+				continue;
+			}
+
+			// If adult product present, exclude ParcelMachine options.
+			if ( $has_adult && 'parcelmachine' === strtolower( $type ) ) {
 				continue;
 			}
 

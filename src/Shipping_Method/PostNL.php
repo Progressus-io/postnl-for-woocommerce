b2dc0a8e569c58a0ef47c09f304f2767d83855af
OlfatProgressus <olfat@progressus.io>
2025-12-17 14:26:10 +0200

    Correctly load the shipping method data

--- src/Shipping_Method/PostNL.php
+++ src/Shipping_Method/PostNL.php
@@ -49,10 +49,6 @@ class PostNL extends \WC_Shipping_Flat_Rate {
 		$this->init_form_fields();
 		$this->init_settings();
 
-		if ( empty( $this->get_option( 'title' ) ) ) {
-			$this->update_option( 'title', $this->method_title );
-		}
-
 		add_filter( 'woocommerce_shipping_instance_form_fields_' . $this->id, array( $this, 'instance_form_fields' ), 10, 1 );
 		add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_shipping_method_assets' ) );
@@ -144,38 +140,18 @@ class PostNL extends \WC_Shipping_Flat_Rate {
 	/**
 	 * Generate Settings HTML.
 	 *
+	 * Delegate to parent for correct instance/global field rendering.
+	 *
 	 * @param array $form_fields Array of form fields.
 	 * @param bool  $echo Echo or return.
-	 * @return string
+	 * @return string|void
 	 */
 	public function generate_settings_html( $form_fields = array(), $echo = true ) {
 		if ( empty( $form_fields ) ) {
 			$form_fields = $this->get_form_fields();
 		}
 
-		$html = '';
-		$settings = Settings::get_instance();
-		
-		foreach ( $form_fields as $k => $v ) {
-			$type = $this->get_field_type( $v );
-
-			// Check if Settings class has a custom generator for this field type
-			if ( method_exists( $settings, 'generate_' . $type . '_html' ) ) {
-				$html .= $settings->{'generate_' . $type . '_html'}( $k, $v );
-			} elseif ( method_exists( $this, 'generate_' . $type . '_html' ) ) {
-				$html .= $this->{'generate_' . $type . '_html'}( $k, $v );
-			} elseif ( has_filter( 'woocommerce_generate_' . $type . '_html' ) ) {
-				$html .= apply_filters( 'woocommerce_generate_' . $type . '_html', '', $k, $v, $this );
-			} else {
-				$html .= $this->generate_text_html( $k, $v );
-			}
-		}
-
-		if ( $echo ) {
-			echo $html; // WPCS: XSS ok.
-		} else {
-			return $html;
-		}
+		return parent::generate_settings_html( $form_fields, $echo );
 	}
 
 }

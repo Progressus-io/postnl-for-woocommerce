41930a032ec50c32e7b9fe8c1b578762b5e25918
OlfatProgressus <olfat@progressus.io>
2026-01-14 17:51:03 +0200

    Ensure ID Check Product Always Triggers Correctly with Signature on Delivery and/or Home Delivery

--- src/Helper/Mapping.php
+++ src/Helper/Mapping.php
@@ -146,6 +146,36 @@ class Mapping {
 							),
 						),
 						array(
+							'combination' => array( 'id_check', 'signature_on_delivery' ),
+							'code'        => '3438',
+							'options'     => array(
+								array(
+									'characteristic' => '002',
+									'option'         => '014',
+								),
+							),
+						),
+						array(
+							'combination' => array( 'id_check', 'only_home_address' ),
+							'code'        => '3438',
+							'options'     => array(
+								array(
+									'characteristic' => '002',
+									'option'         => '014',
+								),
+							),
+						),
+						array(
+							'combination' => array( 'id_check', 'only_home_address', 'signature_on_delivery' ),
+							'code'        => '3438',
+							'options'     => array(
+								array(
+									'characteristic' => '002',
+									'option'         => '014',
+								),
+							),
+						),
+						array(
 							'combination' => array( 'id_check', 'insured_shipping' ),
 							'code'        => '3443',
 							'options'     => array(

a51824a0ea67b9ed6954f50a7834d1216f832866
Abdalsalaam Halawa <abdalsalaam@outlook.com>
2026-02-05 16:41:46 +0200

    v5.9.3 release (#285)

--- postnl-for-woocommerce.php
+++ postnl-for-woocommerce.php
@@ -7,7 +7,7 @@
  * Author URI: https://postnl.post/
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Version: 5.9.2
+ * Version: 5.9.3
  * Requires Plugins: woocommerce
  * Requires PHP: 7.4
  * Requires at least: 6.7
